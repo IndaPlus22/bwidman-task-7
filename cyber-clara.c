26c6e540b881a2e94958c65ea790454d7e0b9f08
Benjaneb <71073919+Benjaneb@users.noreply.github.com>
2022-10-14 01:25:49 +0200

    Broken cyber-clara

--- cyber-clara.c
+++ cyber-clara.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Hello Isak!");
-    
+    int n;
+    scanf("%d", &n);
+
+    // Allocate n strings with 20 characters
+    char** names = malloc(n * 20 * sizeof(char));
+
+    // Name count without duplicates
+    int count = 0;
+
+    // Go through every string and check if it already exists in the array
+    for (int i = 0; i < n; i++) {
+        char* name;
+        scanf("%c", &name);
+
+        _Bool isNew = 1;
+        for (int j = 0; j < i; j++)
+            if (names[j] == name) // Already exists in array
+                isNew = 0;
+        
+        if (isNew)
+            count++;
+    }
+
+    printf("%d", count);
+
     return 0;
 }
