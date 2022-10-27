36561b210edcd15bdff4f9be423d7d7a4d9d79bc
Benjaneb <benjaneb1@gmail.com>
2022-10-27 20:03:40 +0200

    Successfully identifies duplicates

--- cyber-clara.c
+++ cyber-clara.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    // Allocate n strings with 20 characters
-    char** names = malloc(n * 20 * sizeof(char));
+    // Allocate n strings with 20 characters each
+    char** names = malloc(n * 20);
 
     // Name count without duplicates
     int count = 0;
 
     // Go through every string and check if it already exists in the array
     for (int i = 0; i < n; i++) {
-        char* name;
-        scanf("%c", &name);
+        char* name = malloc(20);
+        scanf("%s", name);
+        names[i] = name;
 
         _Bool isNew = 1;
         for (int j = 0; j < i; j++)
-            if (names[j] == name) // Already exists in array
+            if (strcmp(names[j], name) == 0) // Already exists in array
                 isNew = 0;
         
-        if (isNew)
+        if (isNew == 1)
             count++;
     }
 
