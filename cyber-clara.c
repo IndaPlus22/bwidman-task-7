6d9acb2e30ccdf3601c79ba6862459e7c08314f0
Benjaneb <71073919+Benjaneb@users.noreply.github.com>
2022-11-03 00:24:39 +0100

    Implement hashmap for quicker cyber-clara

--- cyber-clara.c
+++ cyber-clara.c
@@ -1,42 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "khash.h"
+
+KHASH_SET_INIT_STR(str)
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    // Allocate n strings with space for two 20 character names seperated by one character each
-    char** names = malloc(n * (20 * 2 + 1));
-
-    // Name count without duplicates
-    int count = 0;
+    // Allocate n strings with space for two 20 character names
+    // seperated by a space and ending with a null character each
+    char** names = malloc(n * (20 * 2 + 2));
 
     // Store all first names
     for (int i = 0; i < n; i++) {
-        char* first_name = malloc(20);
-        scanf("%s", first_name);
+        char* first_name = malloc(21);
+        scanf( "%s", first_name);
         names[i] = first_name;
     }
 
+    // Initialize hashtable
+    khash_t(str)* hashtable = kh_init(str);
+    unsigned int k;
+    int absent;
+
+    // Receive last names, 
     for (int i = 0; i < n; i++) {
-        char* surname = malloc(20);
+        char* surname = malloc(21);
         scanf("%s", surname);
-        
-        strcat(names[i], ",");
+
+        strcat(names[i], " ");
         strcat(names[i], surname);
 
-        // Check every previous full name and say it's new if it doesn't match any
-        _Bool isNew = 1;
-        for (int j = 0; j < i; j++)
-            if (strcmp(names[i], names[j]) == 0) // Already exists in array
-                isNew = 0;
-        
-        if (isNew)
-            count++;
+        kh_put(str, hashtable, names[i], &absent);
     }
 
-    printf("%d", count);
+    printf("%d", kh_size(hashtable));
+
+    free(names);
+    kh_destroy(str, hashtable);
 
     return 0;
 }
