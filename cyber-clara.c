9563bf0b7ebf8422b2fa976c979b4a275d914ccd
Benjaneb <benjaneb1@gmail.com>
2022-11-04 02:23:04 +0100

    Implement new hashtable library

--- cyber-clara.c
+++ cyber-clara.c
@@ -1,45 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "khash.h"
+#include "uthash.h"
 
-KHASH_SET_INIT_STR(str)
+struct NameHash {
+	char* name;
+	int data;
+	UT_hash_handle hh;
+};
 
 int main() {
+    FILE* file = fopen("test.txt", "r");
     int n;
-    scanf("%d", &n);
-
+    fscanf(file, "%d", &n);
+    // scanf("%d", &n);
+    
     // Allocate n strings with space for two 20 character names
     // seperated by a space and ending with a null character each
-    char** names = malloc(n * (20 * 2 + 2));
+    char** names = malloc(n * sizeof(char*));
 
     // Store all first names
     for (int i = 0; i < n; i++) {
-        char* first_name = malloc(21);
-        scanf( "%s", first_name);
+        char* first_name = malloc(22);
+        fscanf(file, "%s", first_name);
+        // scanf("%s", first_name);
         names[i] = first_name;
     }
 
-    // Initialize hashtable
-    khash_t(str)* hashtable = kh_init(str);
-    unsigned int k;
-    int absent;
-
-    // Receive last names, 
+	struct NameHash* hashtable = NULL;
+    
+    // Receive last names
     for (int i = 0; i < n; i++) {
         char* surname = malloc(21);
-        scanf("%s", surname);
+        fscanf(file, "%s", surname);
+        // scanf("%s", surname);
 
         strcat(names[i], " ");
         strcat(names[i], surname);
 
-        kh_put(str, hashtable, names[i], &absent);
+		struct NameHash* nameHash;
+		HASH_FIND_STR(hashtable, names[i], nameHash);
+		if (nameHash == NULL) {
+			nameHash = malloc(sizeof(struct NameHash));
+			nameHash->name = names[i];
+			// nameHash->name = i;
+			nameHash->data = 0;
+			HASH_ADD_KEYPTR(hh, hashtable, nameHash->name, strlen(nameHash->name), nameHash);
+			// HASH_ADD_INT(hashtable, name, nameHash);
+		}
     }
+	// Test: does not duplicate keys
+	// struct NameHash* nameHash;
+	// int a = 1;
+	// HASH_FIND_INT(hashtable, &a, nameHash);
+	// if (nameHash == NULL) {
+	// 	nameHash = malloc(sizeof(struct NameHash));
+	// 	nameHash->name = 1;
+	// 	nameHash->data = 0;
+	// 	HASH_ADD_INT(hashtable, name, nameHash);
+	// }
 
-    printf("%d", kh_size(hashtable));
+    printf("%u", HASH_COUNT(hashtable));
 
     free(names);
-    kh_destroy(str, hashtable);
-
     return 0;
 }
